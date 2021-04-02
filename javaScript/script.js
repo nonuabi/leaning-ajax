57f8a19bc83f5a9fa391ff54023c67c0a761f8a4
nonuabi <nonuabi@gmail.com>
2021-04-02 19:39:48 +0530

    fetching data using jquery

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,16 +1,26 @@
 function fetchRandomDogImage() {
-  var xhrRequest = new XMLHttpRequest();
+  //   var xhrRequest = new XMLHttpRequest();
 
-  xhrRequest.onload = function () {
-    console.log(xhrRequest.response);
-    var responseJson = JSON.parse(xhrRequest.response);
+  //   xhrRequest.onload = function () {
+  //     console.log(xhrRequest.response);
+  //     var responseJson = JSON.parse(xhrRequest.response);
 
-    var imageurl = responseJson.message;
+  //     var imageurl = responseJson.message;
 
-    $("#dog-img").attr("src", imageurl);
-  };
+  //     $("#dog-img").attr("src", imageurl);
+  //   };
 
-  xhrRequest.open("get", "https://dog.ceo/api/breeds/image/random", true);
-  xhrRequest.send();
+  //   xhrRequest.open("get", "https://dog.ceo/api/breeds/image/random", true);
+  //   xhrRequest.send();
+
+  $.ajax({
+    url: "https://dog.ceo/api/breeds/image/random",
+    method: "GET",
+    success: function (data) {
+      var imageURL = data.message;
+      $("#dog-img").attr("src", imageURL);
+    },
+  });
 }
+
 $("#fetch-btn").click(fetchRandomDogImage);
